cdd26b76a54e4a0f6c5e09b7d7d53b7dc1100405
James Shin <hwshinj@gmail.com>
2024-10-01 17:34:25 +1000

    fix: size G not GB

--- hosts/khas/disko.nix
+++ hosts/khas/disko.nix
@@ -9,7 +9,7 @@
                     partitions = {
                         ESP = {
                             type = "EF00";
-                            size = "2GB";
+                            size = "2G";
                             content = {
                                 type = "filesystem";
                                 format = "vfat";
