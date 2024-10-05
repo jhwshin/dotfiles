fa75f8663b4011b6baabe9d92cb4a8814d6e593c
jhwshin <57696372+jhwshin@users.noreply.github.com>
2024-10-06 00:06:28 +1000

    Update hardware-configuration.nix

--- hosts/raszagal/hardware-configuration.nix
+++ hosts/raszagal/hardware-configuration.nix
@@ -36,7 +36,7 @@
   swapDevices = [];
 
   networking.useDHCP = lib.mkDefault true;
-  networking.interfaces.enp1s0.useDHCP = lib.mkDefault = true;
+  networking.interfaces.enp1s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
