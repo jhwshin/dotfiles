33f45cfd8a26da7908b162d76a8947b241269865
James Shin <hwshinj@gmail.com>
2024-10-07 01:38:55 +1100

    disko change

--- hosts/raszagal/disko.nix
+++ hosts/raszagal/disko.nix
@@ -1,74 +1,69 @@
 {
-  disko.devices = {
-    disk = {
-      main = {
-        device = "/dev/vda";
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              label = "boot";
-              type = "EF00";
-              size = "2G";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
-              };
+  disko.devices.disk = {
+    main = {
+      type = "disk";
+      device = "/dev/vda";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            type = "EF00";
+            size = "2G";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "defaults" "umask=0077" ];
             };
-
-            luks = {
-              label = "luks";
-              size = "100%";
+          };
+          luks = {
+            size = "100%";
+            content = {
+              type = "luks";
+              name = "cryptroot";
+              askPassword = true;
+              extraFormatArgs = [
+                "--hash sha512"
+                "--pbkdf argon2id"
+                "--use-random"
+                "--key-size 512"
+                "--pbkdf-memory 1048576"
+                "--pbkdf-parallel 4"
+                "--iter-time 2000"
+                "--cipher aes-xts-plain64"
+              ];
+              extraOpenArgs = [
+                "--perf-no_read_workqueue"
+                "--perf-no_write_workqueue"
+              ];
+              settings = {
+                allowDiscards = true;
+              };
               content = {
-                type = "luks";
-                name = "cryptroot";
-                extraOpenArgs = [
-                  # "--allow-discards"
-                  "--perf-no_read_workqueue"
-                  "--perf-no_write_workqueue"
-                ];
-                settings = {
-                  allowDiscards = true;
-                };
-
-                content = {
-                  type = "btrfs";
-                  extraArgs = [
-                    "-L"
-                    "nixos"
-                    "-f"
-                  ];
-                  subvolumes = {
-                    "/" = {
-                      mountpoint = "/.btrfsroot";
-                      mountOptions = [
-                        "subvolid=5" "defaults" "noatime"
-                      ];
-                    };
-                    "@" = {
-                      mountpoint = "/";
-                      mountOptions = [
-                        "subvol=@" "defaults" "noatime"
-                      ];
-                    };
-                    "@home" = {
-                      mountpoint = "/home";
-                      mountOptions = [
-                        "subvol=@home" "defaults" "noatime"
-                      ];
-                    };
-                    "@nix" = {
-                      mountpoint = "/nix";
-                      mountOptions = [
-                        "subvol=@home" "defaults" "noatime"
-                      ];
-                    };
-                    "@swap" = {
-                      mountpoint = "/.swapvol";
-                      swap.swapfile.size = "1024M";
+                type = "btrfs";
+                extraArgs = [ "-f" ];
+                subvolumes = {
+                  "/" = {
+                    mountpoint = "/.btrfsroot";
+                    mountOptions = [ "subvolid=5" "defaults" "compress=zstd:3" "noatime" "nodiratime" ];
+                  };
+                  "@" = {
+                    mountpoint = "/";
+                    mountOptions = [ "subvol=/@" "defaults" "compress=zstd:3" "noatime" "nodiratime" ];
+                  };
+                  "@home" = {
+                    mountpoint = "/home";
+                    mountOptions = [ "subvol=/@home" "defaults" "compress=zstd:3" "noatime" "nodiratime" ];
+                  };
+                  "@nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [ "subvol=/@nix" "defaults" "compress=zstd:3" "noatime" "nodiratime" ];
+                  };
+                  "@swap" = {
+                    mountpoint = "/.swapvol";
+                    swap.swapfile ={
+                      size = "8G";
+                      path = "swapfile";
                     };
                   };
                 };
