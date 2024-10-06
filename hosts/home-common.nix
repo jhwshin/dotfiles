5b549d9e22db6e9ea07d84550dd6ee041a0f328f
James Shin <hwshinj@gmail.com>
2024-10-07 02:38:02 +1100

    add home packages

---
+++ hosts/home-common.nix
@@ -0,0 +1,33 @@
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+  ];
+
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+    };
+  };
+
+  home = {
+    username = "hws";
+    homeDirectory = "/home/hws";
+  };
+
+  home.packages = with pkgs; [ neovim ];
+
+  programs.home-manager.enable = true;
+  programs.git.enable = true;
+  programs.kitty.enable = true;
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  home.stateVersion = "24.05";
+}
--- hosts/raszagal/home.nix
+++ hosts/raszagal/home.nix
@@ -7,26 +7,10 @@
   ...
 }: {
   imports = [
+    ../home-common.nix
   ];
 
-  nixpkgs = {
-    config = {
-      allowUnfree = true;
-    };
-  };
+  programs.fastfetch.enable = true;
 
-  home = {
-    username = "hws";
-    homeDirectory = "/home/hws";
-  };
-
-  home.packages = with pkgs; [ neovim ];
-
-  programs.home-manager.enable = true;
-  programs.git.enable = true;
-
-  # Nicely reload system units when changing configs
-  systemd.user.startServices = "sd-switch";
-
-  home.stateVersion = "24.05";
 }
+
